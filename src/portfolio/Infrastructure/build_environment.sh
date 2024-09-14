0bf1cfe85e815d44f1755747df723bddfffd24e2
Dan Sayers <contact@dan-sayers.com>
2024-09-15 09:41:31 +1000

    feat: Align with better method to access credentials and re-arrange order of operations

---
+++ src/portfolio/Infrastructure/build_environment.sh
@@ -0,0 +1,14 @@
+# Setup environment
+os_password="$HOME/Portfolio/src/portfolio/local_secrets/portfolio_os_user_secret.txt"
+cd ~
+sudo -S apt install -y python3.12-venv <$os_password
+sudo -S apt install -y python3-pip <$os_password
+python3 -m venv portfolio_venv
+source ./portfolio_venv/bin/activate
+python3 -m pip install --upgrade setuptools wheel pip
+pip install -r "$HOME/Portfolio/src/portfolio/requirements.txt"
+#todo turning on dagster goes here?
+deactivate
+
+# Fetch codebase - #todo if running locally/developing remember to comment out
+#git clone git@github.com:realitybytez/Portfolio.git
