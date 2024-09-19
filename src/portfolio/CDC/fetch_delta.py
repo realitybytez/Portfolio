ec7b6ab033c6365ccd4247ad944d157732b544d0
Dan Sayers <contact@dan-sayers.com>
2024-09-19 21:24:42 +1000

    feat: Finished cdc to bronze functionality Data is now captured as a daily delta in sync with the upstream practice of dumping each day of data to flat file then uploading to the replica database.
    This identifies the delta and pulls it out of the database. State
    is now retained so the script knows which day it is up to and
    will detect if a local "bronze" environment (e.g landing/raw)
    does not exist.
    
    The script is now also integrated properly with the rest of the
    project in terms of secrets management and the database to use

--- src/portfolio/CDC/fetch_delta.py
+++ src/portfolio/CDC/fetch_delta.py
@@ -1,53 +1,98 @@
 from psycopg2 import connect
 import yaml
-from secret import secret
 from SQL import sql
 from datetime import datetime
 import pyarrow
 import pyarrow.parquet as parquet
+import os
+import os.path as path
+import pickle
 
-with open('config.yml', 'r') as file:
-    cfg = yaml.safe_load(file)['db_params']
+base_path = os.path.join(os.path.expanduser("~"), 'Portfolio/src/portfolio/CDC')
+parent_path = os.path.join(os.path.expanduser("~"), 'Portfolio/src')
+run_time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 
-db_pyarrow_mapping = {
-    'character varying': pyarrow.string(),
-    'integer': pyarrow.int32(),
-    'timestamp without time zone': pyarrow.timestamp('s'),
-    'numeric': pyarrow.decimal128(12, 2)
-}
+def get_job_state(metadata):
 
-conn = connect(user=cfg['user'], password=secret['postgres_password'], host=cfg['host'], port=cfg['port'], database=cfg['database'])
+    if os.path.exists(path.join(base_path, 'job_state.pkl')):
+        with open(path.join(base_path, 'job_state.pkl'), 'rb') as f:
+            state = pickle.load(f)
+    else:
+        # Just needs to be any time before policy forge go live, how about unix epoch
+        table_names = [m[0] for m in metadata]
+        state = dict()
+        for t in table_names:
+            state[t] = {'last_modified_datetime': datetime(year=1970, month=1, day=1, hour=0, minute=0, second=0)}
+    return state
 
-# todo temp solution until on cloud WH infra
-last_query_time = datetime(year=2006, month=3, day=25, hour=0, minute=0, second=0)
+def set_job_state(state):
+    with open(path.join(base_path, 'job_state.pkl'), 'wb') as file:
+        pickle.dump(state, file)
 
-cursor = conn.cursor()
-cursor.execute(sql['get_table_metadata'])
-results = cursor.fetchall()
+def get_secret(cfg):
+    with open(path.join(parent_path, cfg['secret_store'])) as f:
+        secret = f.readline().strip()
+    return secret
 
-table_names = {x[0] for x in results}
-schemas = dict()
-seen_tables = set()
-for result in results:
-    if result[0] not in seen_tables:
-        schemas[result[0]] = []
-        seen_tables.add(result[0])
-    schemas[result[0]].append((result[1], db_pyarrow_mapping[result[2]]))
+def get_cfg():
+    with open(f'{base_path}/config.yml', 'r') as file:
+        cfg = yaml.safe_load(file)['db_params']
+    return cfg
 
-for schema_name, schema_data in schemas.items():
-    query = sql['get_table_delta'].format(schema_name, last_query_time)
-    cursor.execute(query)
-    table = cursor.fetchall()
-    schema = pyarrow.schema(schema_data)
-    columns = list(zip(*table))
+def get_delta():
+    cfg = get_cfg()
 
-    dict_arrays = dict()
-    for i, data in enumerate(schema_data):
-        dict_arrays[data[0]] = columns[i]
+    db_pyarrow_mapping = {
+        'character varying': pyarrow.string(),
+        'integer': pyarrow.int32(),
+        'timestamp without time zone': pyarrow.timestamp('s'),
+        'numeric': pyarrow.decimal128(12, 2)
+    }
 
-    table = pyarrow.Table.from_pydict(dict_arrays, schema)
+    if not os.path.exists(f'{parent_path}/bronze/sources/policy_forge/{run_time}'):
+        os.makedirs(f'{parent_path}/bronze/sources/policy_forge/{run_time}')
+    print('make', f'{parent_path}/bronze/sources/policy_forge/{run_time}')
 
-    # todo temp solution until on cloud WH infra
-    parquet.write_table(table, f'{schema_name}.parquet')
+    conn = connect(user=cfg['user'], password=get_secret(cfg), host=cfg['host'], port=cfg['port'], database=cfg['database'])
 
+    cursor = conn.cursor()
+    cursor.execute(sql['get_table_metadata'])
+    metadata = cursor.fetchall()
 
+    schemas = dict()
+    seen_tables = set()
+    for result in metadata:
+        if result[0] not in seen_tables:
+            schemas[result[0]] = []
+            seen_tables.add(result[0])
+        schemas[result[0]].append((result[1], db_pyarrow_mapping[result[2]]))
+
+    state = get_job_state(metadata)
+
+    for schema_name, schema_data in schemas.items():
+        print('Last modified is for', schema_name, 'is', state[schema_name]['last_modified_datetime'])
+        query = sql['get_table_delta'].format(schema_name, state[schema_name]['last_modified_datetime'])
+        cursor.execute(query)
+        table = cursor.fetchall()
+        if len(table) == 0:
+            continue
+        schema = pyarrow.schema(schema_data)
+        columns = list(zip(*table))
+
+        dict_arrays = dict()
+        for i, data in enumerate(schema_data):
+            dict_arrays[data[0]] = columns[i]
+
+        table = pyarrow.Table.from_pydict(dict_arrays, schema)
+
+        query = sql['get_last_modified'].format(schema_name, state[schema_name]['last_modified_datetime'])
+        cursor.execute(query)
+        result = cursor.fetchall()
+        state[schema_name]['last_modified_datetime'] = result[0][0]
+        print('state for', schema_name, 'is now', result[0][0])
+
+        # todo temp solution until on cloud WH infra
+
+        parquet.write_table(table, f'{parent_path}/bronze/sources/policy_forge/{run_time}/{schema_name}.parquet')
+        print('write', f'{parent_path}/bronze/sources/policy_forge/{run_time}/{schema_name}.parquet')
+    set_job_state(state)
