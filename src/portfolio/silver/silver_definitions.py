0c7e9f08f87f1132002414977243914f84c6405a
Dan Sayers <contact@dan-sayers.com>
2025-03-06 21:31:23 +1100

    feat: DBT assets can now be run from Dagster using the integration

--- src/portfolio/silver/silver_definitions.py
+++ src/portfolio/silver/silver_definitions.py
@@ -1,21 +1,25 @@
 from pathlib import Path
-
 from dagster import AssetExecutionContext, Definitions
 from dagster_dbt import (
     DbtCliResource,
     DbtProject,
-    build_schedule_from_dbt_selection,
     dbt_assets,
 )
+import os
+import json
 
 dbt_silver = DbtProject (
-project_dir= "Portfolio/src/portfolio/silver",
-target='prod'
+    project_dir= Path(__file__).joinpath("..").resolve(),
+    target='dev'
 )
 
 @dbt_assets(manifest=dbt_silver.manifest_path)
 def silver_assets(context: AssetExecutionContext, dbt: DbtCliResource):
-    yield from dbt.cli(["build"], context=context).stream()
+    # seems like only way to get secret into dbt process when invoked from dagster... works fine from CLI just not
+    # when dagster kicks it off.... come back at later date to revise as this is not ideal.
+    # hard coded values are read from dbt profiles file just fine...
+    dbt_vars = {"DBT_PASSWORD": os.getenv('DBT_PASSWORD')}
+    yield from dbt.cli(["build","--vars", json.dumps(dbt_vars)], context=context).stream()
 
 
 defs = Definitions(
@@ -24,3 +28,5 @@ defs = Definitions(
         "dbt": DbtCliResource(project_dir=dbt_silver),
     },
 )
+
+x=0
